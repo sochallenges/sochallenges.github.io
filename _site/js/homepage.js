99db19497e5bee466b75c8d5711843f60c17bbce
Alice Yeh <aliceyeh12@gmail.com>
2020-04-14 16:49:24 -0700

    Post final challenge

--- _site/js/homepage.js
+++ _site/js/homepage.js
@@ -3,14 +3,22 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Apr 7, 2020 016:15:00').getTime(),
+let countDown = new Date('Apr 14, 2020 016:15:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
           distance = countDown - now;
 
+    /*
       document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    */
+
+   document.getElementById('days').innerText = 0,
+   document.getElementById('hours').innerText = 0,
+   document.getElementById('minutes').innerText = 0,
+   document.getElementById('seconds').innerText = 0;
+
     }, second)
--- js/homepage.js
+++ js/homepage.js
@@ -9,8 +9,16 @@ let countDown = new Date('Apr 14, 2020 016:15:00').getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
+    /*
       document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    */
+
+   document.getElementById('days').innerText = 0,
+   document.getElementById('hours').innerText = 0,
+   document.getElementById('minutes').innerText = 0,
+   document.getElementById('seconds').innerText = 0;
+
     }, second)
